ca61db91c296b25d43ce0d0c91ff4285f84d86bc
Matheus Lima <75711232+MathsLima@users.noreply.github.com>
2023-09-17 00:24:23 -0300

    Java

---
+++ while/numerosPares.java
@@ -0,0 +1,14 @@
+
+/**
+* Faça um programa que escreve todos os pares entre 1000 e 2000.
+ */
+public class numerosPares
+{
+   public static void main (String args[]){
+       int num = 1000;
+       while(num <= 2000){
+           num = num+2;
+           System.out.println(num);
+       }
+   }
+}
